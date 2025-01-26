9d08664ed38ae2ecaddf93182fc759fbedc55750
Aloof-Llama <strawberryshake@gmx.net>
2025-01-26 12:24:25 +0100

    added counting words

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+def count_words(text):
+    word_list = text.split()
+    word_count = len(word_list)
+    return word_count
+
 def main():
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        word_count = count_words(file_contents)
+        print(word_count)
 main()
