8cfdb4537e28062adef63d11907a0a1b7b4ac8d7
Aloof-Llama <strawberryshake@gmx.net>
2025-01-26 20:25:07 +0100

    separate get text function

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 def count_words(text):
     word_list = text.split()
     word_count = len(word_list)
@@ -10,12 +14,26 @@ def count_characters(text):
         char_counts[letter] = char_counts.get(letter, 0) + 1
     return char_counts
 
+def sort_on(dict):
+    return dict["num"]
+
+def get_character_list(character_count):
+    character_list = []
+    for character, count in character_count.items():
+        character_dictionary = {"char": character, "num": count}
+        character_list.append(character_dictionary)
+    character_list.sort(reverse = True, key = sort_on)
+    return character_list
+
+
 
 def main():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        word_count = count_words(file_contents)
-        print(f"total words: {word_count}")
-        character_count = count_characters(file_contents)
-        print(f"character count: {character_count}")
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = count_words(text)
+    print(f"total words: {word_count}")
+    character_count = count_characters(text)
+    print(f"character count: {character_count}")
+    print(character_count.keys())
+
 main()
