89bd487345d5786c0e9b4f26a606f9f2cc27d140
Aloof-Llama <strawberryshake@gmx.net>
2025-01-26 20:41:15 +0100

    added make report and checked for alphabet letters

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def count_words(text):
 def count_characters(text):
     char_counts = {}
     for char in text:
-        letter = char.lower()
-        char_counts[letter] = char_counts.get(letter, 0) + 1
+        if char.isalpha():
+            letter = char.lower()
+            char_counts[letter] = char_counts.get(letter, 0) + 1
     return char_counts
 
 def sort_on(dict):
@@ -25,15 +26,22 @@ def get_character_list(character_count):
     character_list.sort(reverse = True, key = sort_on)
     return character_list
 
+def make_report(path, character_list, word_count):
+    print(f"--- Begin report of {path}")
+    print(f"{word_count} words found in the document\n")
+    for character in character_list:
+        char = character["char"]
+        num = character["num"]
+        print(f"The '{char}' character was found {num} times")
+
 
 
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
     word_count = count_words(text)
-    print(f"total words: {word_count}")
     character_count = count_characters(text)
-    print(f"character count: {character_count}")
-    print(character_count.keys())
+    character_list = get_character_list(character_count)
+    make_report(book_path, character_list, word_count)
 
 main()
